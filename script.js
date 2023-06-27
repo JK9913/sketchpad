926ca4f6da4ef6b632e8726e8ed3fb346149bb25
Jan-Kristian Karlsen <jan-kristiankarlsen@jankristiansmbp.lan>
2023-06-27 21:07:33 +0200

    Created a delete grid function which deletes all childs of the squares and then the dquares themselves. User can now generate any amount of squares in the grid and the functionality persists and grid is deleted and created perfectly.

--- script.js
+++ script.js
@@ -12,26 +12,48 @@ function createGrid(number) {
             tempDiv.appendChild(tempSquare);
         }
         container.appendChild(tempDiv);
+    
+    // select all squars and colors black on hover
+    const column = document.querySelectorAll('.square');
+
+    column.forEach(e => {
+    e.onmouseover = function () {
+        this.style.backgroundColor = 'black';
+    }
+    //e.addEventListener('mouseover', () => e.style.backgroundcolor = 'black')
+    });
     }
 }
+
+// Function for deleting a grid before placing a new one
+function deleteGrid() {
+    const squaresToDelete = document.querySelectorAll('.squares');
+
+    squaresToDelete.forEach(x => {
+        let child = x.lastElementChild;
+        while (child){
+            x.removeChild(child);
+            child = x.lastElementChild;
+        }
+        x.remove()
+    })
+}
+
 
 // Create standard grid when page loads
 createGrid(16);
 
 
-
-
-
-
-// select all squars and colors black on hover
-const column = document.querySelectorAll('.square');
-
-column.forEach(e => {
-    e.onmouseover = function () {
-        this.style.backgroundColor = 'black';
+// Add eventlistener to button
+// Should generate new grid with entered amount of cells
+const button = document.querySelector('button')
+const numberField = document.querySelector('input')
+button.addEventListener('click', () => {
+    console.log(numberField);
+    if (!(numberField.value === null) && !(numberField.value === 0)) {
+        console.log('Creating grid...');
+        deleteGrid();
+        createGrid(numberField.value);
     }
-    //e.addEventListener('mouseover', () => e.style.backgroundcolor = 'black')
-});
+})
 
-
-//
