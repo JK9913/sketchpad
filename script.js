d60b57f0ffe09a0c5f6fb4db1bb3f0392e315112
Jan-Kristian Karlsen <jan-kristiankarlsen@jankristiansmbp.lan>
2023-06-27 08:53:33 +0200

    created the real html file with only the top container. javascript now updated with nested for loops to create grid, with input number to decode how big the grid should be. css unutouched

--- script.js
+++ script.js
@@ -3,9 +3,20 @@ const container = document.querySelector('.grid');
 // Start creating the grid which contains 16x16 divs
 // Should be a function which takes in the number of squares
 function createGrid(number) {
-    
+    for (let i = 0; i<number; i++) {
+        let tempDiv = document.createElement('div');
+        tempDiv.classList.add('squares');
+        for (let i = 0; i<number;i++){
+            let tempSquare = document.createElement('div');
+            tempSquare.classList.add('square');
+            tempDiv.appendChild(tempSquare);
+        }
+        container.appendChild(tempDiv);
+    }
 }
 
+// Create standard grid when page loads
+createGrid(16);
 
 
 
@@ -16,7 +27,6 @@ function createGrid(number) {
 const column = document.querySelectorAll('.square');
 
 column.forEach(e => {
-    console.log(e)
     e.onmouseover = function () {
         this.style.backgroundColor = 'black';
     }
